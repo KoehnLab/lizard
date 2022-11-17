3be1b965f2faa32d5ded052996cf5dc1a8593e73
Robert Adam <dev@robert-adam.de>
2022-11-17 13:29:26 +0100

    Remove dummy code

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,3 @@
-#include "lizard/core/Exception.hpp"
-#include "lizard/parser/GeCCoExportParser.hpp"
-
-#include <exception>
-
 int main() {
-	try {
-		lizard::parser::GeCCoExportParser::parse(
-			"/home/robert/Documents/University/Documents/Masterarbeit/Git/program/samples/CC2/CC2_EN.EXPORT");
-
-		return 0;
-	} catch (const std::exception &e) {
-		lizard::core::print_exception_message(e);
-	}
+	return 0;
 }
