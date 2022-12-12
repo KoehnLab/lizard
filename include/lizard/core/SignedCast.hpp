f7c83731cad1f7f6cf9193e8073eed518eaf12df
Robert Adam <dev@robert-adam.de>
2022-12-12 15:12:24 +0100

    Implement signed_cast

---
+++ include/lizard/core/SignedCast.hpp
@@ -0,0 +1,58 @@
+// This file is part of the lizard quantum chemistry software.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file at the root of the lizard source
+// tree or at <https://github.com/Krzmbrzl/lizard/blob/main/LICENSE>.
+
+#ifndef LIZARD_CORE_SIGNED_CAST_HPP_
+#define LIZARD_CORE_SIGNED_CAST_HPP_
+
+#include <cstring>
+#include <type_traits>
+
+namespace lizard {
+
+/**
+ * This function implements a conversion between the signed and unsigned variant of a given integer type.
+ * The function is intended to be used for such casts as the regular static_cast has implementation-defined
+ * behavior when e.g. casting from an unsigned type to the signed version, if the value represented by the
+ * unsigned type can't be represented by the signed type (e.g. 255 as an std::int8_t).
+ * This function always performs a conversion such that the binary representation of the integer value remains
+ * unchanged. Therefore, it is always guaranteed that when casting e.g. from signed -> unsigned -> signed, the
+ * same value is regained (the same also holds in the other direction).
+ * Furthermore, the function guarantees that when converting a negative value to an unsigned type, the result
+ * will represent a positive value, that is larger than the biggest representable number of the signed type.
+ * Likewise, positive numbers that are representable by the signed type are guaranteed to retain their
+ * represented value (e.g. unsigned_value == signed_cast< signed_type >(unsigned_value)).
+ */
+template< typename To, typename From > auto signed_cast(From value) -> To {
+	static_assert(sizeof(To) == sizeof(From), "signed_cast can only convert between types of equal bit width");
+	static_assert(std::is_integral_v< From > && std::is_integral_v< To >,
+				  "signed_cast can only be applied to integral types");
+	static_assert(std::is_signed_v< From > != std::is_signed_v< To >,
+				  "signed_cast can not be used on two types with equal signedness");
+	// In theory, the claims made in the function's description should also hold for other
+	// integer representations. However, before we allow them, we have to perform more
+	// extensive testing.
+	static_assert(-1 == ~0, "signed_cast only implemented for two's complement architecture");
+
+	// Note: Using memcpy seems a little too much overhead for such an operation. However, the vast
+	// majority of compilers should optimize the memcpy call out.
+
+	if constexpr (std::is_signed_v< From >) {
+		// Convert to unsigned
+		std::make_unsigned_t< From > convertedValue;
+		std::memcpy(&convertedValue, &value, sizeof(From));
+
+		return convertedValue;
+	} else {
+		// Convert to signed
+		std::make_signed_t< From > convertedValue;
+		std::memcpy(&convertedValue, &value, sizeof(From));
+
+		return convertedValue;
+	}
+}
+
+} // namespace lizard
+
+#endif // LIZARD_CORE_SIGNED_CAST_HPP_
---
+++ tests/core/SignedCastTest.cpp
@@ -0,0 +1,103 @@
+// This file is part of the lizard quantum chemistry software.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file at the root of the lizard source
+// tree or at <https://github.com/Krzmbrzl/lizard/blob/main/LICENSE>.
+
+#include "lizard/core/SignedCast.hpp"
+
+#include <gtest/gtest.h>
+
+#include <cstdint>
+#include <limits>
+#include <type_traits>
+
+template< typename Int, std::make_signed_t< Int > iterations > void test_signed_cast() {
+	using SignedInt   = std::make_signed_t< Int >;
+	using UnsignedInt = std::make_unsigned_t< Int >;
+
+	constexpr const SignedInt increment = std::numeric_limits< SignedInt >::max() / iterations;
+	static_assert(increment > 0);
+
+	SignedInt signedInt     = 0;
+	UnsignedInt unsignedInt = 0;
+
+	for (SignedInt i = 0; i <= iterations; ++i) {
+		if (i == iterations) {
+			// Ensure we test the max. value
+			signedInt = std::numeric_limits< SignedInt >::max();
+		} else {
+			signedInt = i * increment;
+		}
+
+
+		// For positive numbers the signed and unsigned type should be identical
+		unsignedInt = lizard::signed_cast< UnsignedInt >(signedInt);
+		ASSERT_EQ(static_cast< UnsignedInt >(signedInt), unsignedInt);
+		ASSERT_EQ(signedInt, lizard::signed_cast< SignedInt >(unsignedInt));
+
+		if (signedInt > 0) {
+			// For negative numbers we require that the corresponding unsigned representation is bigger
+			// than any positive number that can be represented with the signed type.
+			signedInt *= -1;
+			unsignedInt = lizard::signed_cast< UnsignedInt >(signedInt);
+			ASSERT_GT(unsignedInt, static_cast< UnsignedInt >(std::numeric_limits< decltype(signedInt) >::max()));
+			ASSERT_EQ(signedInt, lizard::signed_cast< SignedInt >(unsignedInt));
+		}
+
+		if (i == iterations) {
+			// In case iterations == max. representable value, the regular for-loop logic would produce an endless loop
+			break;
+		}
+	}
+
+	for (SignedInt i = 0; i <= iterations; ++i) {
+		if (i == iterations) {
+			// Ensure we test the max. value
+			unsignedInt = std::numeric_limits< UnsignedInt >::max();
+		} else if (i == 0) {
+			// Ensure we test the value just outside the range of the signed type
+			unsignedInt = static_cast< UnsignedInt >(std::numeric_limits< SignedInt >::max()) + 1;
+		} else {
+			unsignedInt = static_cast< UnsignedInt >(std::numeric_limits< SignedInt >::max()) + i * increment;
+		}
+
+		// For unsigned values that are bigger than any positive value representable with the signed
+		// correspondent, we expect the conversion to signed values to end up as a negative number (< 0)
+		signedInt = lizard::signed_cast< SignedInt >(unsignedInt);
+
+		ASSERT_LT(signedInt, 0);
+		ASSERT_EQ(unsignedInt, lizard::signed_cast< UnsignedInt >(signedInt));
+
+		if (i == iterations) {
+			// In case iterations == max. representable value, the regular for-loop logic would produce an endless loop
+			break;
+		}
+	}
+}
+
+constexpr auto pow(std::int64_t base, std::int64_t exponent) -> std::int64_t {
+	std::int64_t result = 1;
+	for (std::int64_t i = 0; i < exponent; ++i) {
+		result *= base;
+	}
+
+	return result;
+}
+
+static constexpr const int nIterations = std::numeric_limits< std::int8_t >::max();
+
+TEST(Core, signed_cast_int8) {
+	test_signed_cast< std::int8_t, nIterations >();
+}
+
+TEST(Core, signed_cast_int16) {
+	test_signed_cast< std::int16_t, nIterations >();
+}
+
+TEST(Core, signed_cast_int32) {
+	test_signed_cast< std::int32_t, nIterations >();
+}
+
+TEST(Core, signed_cast_int64) {
+	test_signed_cast< std::int64_t, nIterations >();
+}
