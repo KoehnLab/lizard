17a11cd7d1b3ac76849b74f2b3ffadb7a866ffa2
Robert Adam <dev@robert-adam.de>
2023-04-13 16:37:07 +0200

    Added typedefs for tensor expressions

---
+++ include/lizard/symbolic/TensorExpressions.hpp
@@ -0,0 +1,17 @@
+// This file is part of the lizard quantum chemistry software.
+// Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file at the root of the lizard source
+// tree or at <https://github.com/KoehnLab/lizard/blob/main/LICENSE>.
+
+#pragma once
+
+#include "lizard/core/Expression.hpp"
+#include "lizard/core/ExpressionTree.hpp"
+#include "lizard/symbolic/TensorElement.hpp"
+
+namespace lizard {
+
+using TensorExpr     = Expression< TensorElement >;
+using TensorExprTree = ExpressionTree< TensorElement >;
+
+} // namespace lizard
